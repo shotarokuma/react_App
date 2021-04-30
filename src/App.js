f8c5b498bb20c0e5e09d0b02c5ab3a7f5760473e
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2021-04-30 18:23:12 +0900

    [learn]jsx syntax Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
-}
+import React ,{Component } from "react";
 
+class App extends Component {
+  render() { 
+    // const greeting = "Bye"
+    // const dom = <h1 className = "foo">{greeting}  Bye world</h1>;
+    return(
+      <React.Fragment>
+        <label htmlFor ="bar">bar</label>
+        <input type = "text" onClick ={() => {console.log("my clicked")}} onChange = {() => {console.log("hi")}}/>
+      </React.Fragment>
+    )
+    // const dom = <input type = "text" onClick ={() => {console.log("my clicked")}} onChange = {() => {console.log("hi")}}/>
+    // return dom
+  }
+}
 export default App;
