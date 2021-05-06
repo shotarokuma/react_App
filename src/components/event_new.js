a299b1ec78dc6c89a5cd5b939fb1cc63fb611242
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2021-05-06 16:01:06 +0900

    button updata

--- src/components/event_new.js
+++ src/components/event_new.js
@@ -23,7 +23,8 @@ class EventsNew extends Component {
   }
 
   render() {
-    const { handleSubmit } = this.props
+    const { handleSubmit, pristine, submitting } = this.props
+
     return (
       <form onSubmit = {handleSubmit(this.onSubmit)}>
        <div>
@@ -33,7 +34,7 @@ class EventsNew extends Component {
          <Field label = "Body" name = "body" type = "text" component = {this.renderField}/>
        </div>
        <div>
-         <input type ="submit" value = "Submit" disable = { false }/>
+         <input type ="submit" value = "Submit" disabled = {pristine || submitting}/>
          <Link to="/">Cancel</Link>
        </div>
       </form>
